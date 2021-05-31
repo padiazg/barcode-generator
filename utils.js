3e06a389e57365abcb5b2ad74c3024e9695b9801
Patricio Diaz <padiazg@gmail.com>
2021-05-31 17:58:38 -0400

    Fixed some typos. Reworked some code. Added live page example
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

---
+++ utils.js
@@ -0,0 +1,20 @@
+// converts a Buffer to a Stream
+const toStream = buffer => {
+        const stream = new Duplex()
+        stream.push(buffer);
+        stream.push(null);
+        return stream
+    } // toStream ...
+
+// converts a Stream to a Buffer
+const toBuffer = stream => new Promise((resolve, reject) => {
+    let b0 = [];
+    stream.on('error', reject);
+    stream.on('data', data => b0.push(data));
+    stream.on('end', () => resolve(Buffer.concat(b0)));
+}); // toBuffer ...
+
+module.export = {
+    toStream,
+    toBuffer,
+}
